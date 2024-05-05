6e0e978d3fd139665d8113bb48c89ffcf787608d
Edoardo Baravaglio <edo.baravaglio@gmail.com>
2024-05-05 16:49:59 +0200

    init

---
+++ frontend/app/layout.tsx
@@ -0,0 +1,31 @@
+import Navbar from "@/components/Navbar";
+import AuthProvider from "@/providers/AuthProvider";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Listo",
+  description:
+    "Elevate your dining experience with Listo! Effortlessly manage your restaurant wishlist and track your culinary adventures.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        <AuthProvider>
+          <Navbar />
+          {children}
+        </AuthProvider>
+      </body>
+    </html>
+  );
+}
+
---
+++ frontend/tailwind.config.ts
@@ -0,0 +1,44 @@
+import type { Config } from "tailwindcss"
+
+const config = {
+  darkMode: ["class"],
+  content: [
+    './pages/**/*.{ts,tsx}',
+    './components/**/*.{ts,tsx}',
+    './app/**/*.{ts,tsx}',
+    './src/**/*.{ts,tsx}',
+  ],
+  prefix: "",
+  theme: {
+    container: {
+      center: true,
+      padding: "0.5rem",
+      screens: {
+        /* '2xl': '1850px',
+        xl: '1440px',
+        lg: '1100px',
+        md: '768px', */
+        sm: '640px',
+      },
+    },
+    extend: {
+      keyframes: {
+        "accordion-down": {
+          from: { height: "0" },
+          to: { height: "var(--radix-accordion-content-height)" },
+        },
+        "accordion-up": {
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: "0" },
+        },
+      },
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
+      },
+    },
+  },
+  plugins: [require("tailwindcss-animate")],
+} satisfies Config
+
+export default config
